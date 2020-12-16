8eb65476161ef77cdcd86fc88068352f79517bc0
Ankita Gupta <ankita.d.gupta@capgemini.com>
2020-12-16 22:38:16 +0530

    Initial Commit

---
+++ DynamicTable/DynamicTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicTable
+//
+//  Created by Ankita  on 16/12/20.
+//  Copyright © 2020 Ankita . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
